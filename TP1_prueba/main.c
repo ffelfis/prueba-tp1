7e7c6cd5f0624ed911c162ed18490c3f659d87eb
ffelfis <ffelfis@gmail.com>
2018-10-06 18:31:46 -0300

    Agrega la compilación e archivo ejecutable en WINDOWS 10
    Luego de installar MinGW se puede compilar el códifo en c.
    Agrega los casos de prueba: archivos .txt.

--- TP1_prueba/main.c
+++ TP1_prueba/main.c
@@ -13,20 +13,19 @@
 
 void mostrar_error_y_salir(char *mensaje_error, int numero_salida){
     fprintf(stderr, "%s\n", mensaje_error);
- //   exit(numero_salida);
+    exit(numero_salida);
 }
 
-void mostrar_version(){ printf("%s\n", VERSION);}
+void mostrar_version(){printf("%s\n", VERSION);}
 
-void mostrar_ayuda(){
-    printf("%s\n", MENSAJE_AYUDA);
-}
+void mostrar_ayuda(){printf("%s\n", MENSAJE_AYUDA);}
 
 int obtener_longitud_maxima(FILE* archivo){
+    //Obtiene longitud contando el \n o \0 del final
+    // ejemplo: para "hola", maximo = 5
     int maximo = 0;
     int actual = 0;
     int caracter;
-    //while (caracter = fgetc(archivo) != EOF){
     while ((caracter = fgetc(archivo)) != EOF){
         actual++;
         if (caracter == '\n'){
@@ -39,31 +38,44 @@ int obtener_longitud_maxima(FILE* archivo){
 
 int obtener_cantidad_palabras(FILE *archivo, int len){
     int cantidad = 0;
-    char palabra[len];
-    while(fgets(palabra, len, archivo)) cantidad ++;
+    char c = ' ';
+    while (c != EOF){
+        c = fgetc(archivo);
+        if (c == '\n') cantidad ++;
+    }
+    //printf("%i\n", cantidad);
     return cantidad;
 }
 
-void obtener_palabras(FILE* archivo_a_ordenar, char **lista_palabras, int len){
+void obtener_palabras(FILE* archivo, char **lista_palabras, int len, int cantidad_palabras){
     int indice = 0;
-    char palabra[len];
+    char palabra[len + 1];
 
-    while(fgets(palabra, len, archivo_a_ordenar) != NULL){
-        *(palabra + strlen(palabra) - 1) = '\0';
-        lista_palabras[indice] = malloc(strlen(palabra));
+    for (int i = 0; i < cantidad_palabras; i++){
+        fgets(palabra, len + 1, archivo);
+        for(int j = 0; j < (strlen(palabra) + 1); j++){
+            if (palabra[j] == '\n' || palabra[j] == EOF) palabra[j] = '\0';
+        }
+        lista_palabras[indice] = malloc(strlen(palabra) + 1);
         strcpy(lista_palabras[indice], palabra);
         indice ++;
     }
 }
 
-void orgaqsort(char** izq, char** der){
-    if (izq < der) {
+int comparar_como_numero(const char *numero1,const char *numero2){
+    int n1 = atoi(numero1);
+    int n2 = atoi(numero2);
+    return(n1-n2);
+}
+
+void orgaqsortgeneral(char** izq, char** der, int (*fcmp)(const char *,const char *)){
+    if (izq <= der) {
         char *aux;
         char **inicio = izq;
         char **fin = der;
         while (inicio < fin){
-            while (strcmp(*inicio, *izq) <= 0)inicio++; //falta hacer que compare enteros, que seria
-            while (strcmp(*fin, *izq) > 0) fin--;       //atoi(*inicio) <=/> atoi(*izq), y tambien hacer qsort general
+            while ((fcmp(*inicio, *izq) <= 0) && (inicio < der))inicio++;
+            while ((fcmp(*fin, *izq) > 0) && (fin > izq)) fin--;
             if (inicio < fin){
                 aux = *inicio;
                 *inicio = *fin;
@@ -73,10 +85,16 @@ void orgaqsort(char** izq, char** der){
         aux = *izq;
         *izq = *fin;
         *fin = aux;
-        orgaqsort(izq, fin-1);
-        orgaqsort(fin+1, der);
+        orgaqsortgeneral(izq, fin-1, fcmp);
+        orgaqsortgeneral(fin+1, der, fcmp);
     }
+}
 
+void orgaqsort(char **izq, char **der, int num){
+    //si num = 0 ordena alfabeticamente
+    //Si num != 0 ordena como numeros
+    if (num) orgaqsortgeneral(izq, der, comparar_como_numero);
+    else orgaqsortgeneral(izq, der, strcmp);
 }
 
 int main(int argc, char *argv[]){
@@ -89,25 +107,25 @@ int main(int argc, char *argv[]){
     if ((argc < 2) || argc > 5) {
         mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
     }
-    if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)){
+    else if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)){
         if(argc != 2) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         mostrar_ayuda();
         return 0;
     }
-    if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)){
+    else if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)){
         if(argc != 2) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         mostrar_version();
         return 0;
     }
 
-    if ((strcmp(argv[1], "-o") == 0) || (strcmp(argv[1], "--output") == 0)){
+    else if ((strcmp(argv[1], "-o") == 0) || (strcmp(argv[1], "--output") == 0)){
         if (argc != 4) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         nro_archivo_entrada = 3;
         nro_archivo_salida = 2;
         criterio_ordenamiento = 0;
     }
 
-    if (((strcmp(argv[2], "-o") == 0) || (strcmp(argv[2], "--output") == 0)) && ((strcmp(argv[1], "-n") == 0) || (strcmp(argv[1], "--numeric") == 0))) {
+    else if (((strcmp(argv[2], "-o") == 0) || (strcmp(argv[2], "--output") == 0)) && ((strcmp(argv[1], "-n") == 0) || (strcmp(argv[1], "--numeric") == 0))) {
         if (argc != 5) mostrar_error_y_salir(mensaje_error_cantidad_parametros, salida_error_parametros);
         nro_archivo_entrada = 4;
         nro_archivo_salida = 3;
@@ -122,16 +140,16 @@ int main(int argc, char *argv[]){
         output = fopen(argv[nro_archivo_salida], "w");
         if (!output) mostrar_error_y_salir(mensaje_error_archivo_inexistente, salida_error_archivo_inexistente);
     }
+
     longitud = obtener_longitud_maxima(archivo);
     rewind(archivo);
     cantidad_palabras = obtener_cantidad_palabras(archivo, longitud);
     rewind(archivo);
     lista_palabras = malloc(cantidad_palabras * sizeof(char*));
-    obtener_palabras(archivo, lista_palabras, longitud);
-    orgaqsort(lista_palabras, lista_palabras + cantidad_palabras -1);
+    obtener_palabras(archivo, lista_palabras, longitud, cantidad_palabras);
+    orgaqsort(lista_palabras, lista_palabras + cantidad_palabras -1, criterio_ordenamiento);
     for (int i = 0; i < cantidad_palabras; i++){
-        fprintf(output, lista_palabras[i]);
-        fprintf(output, "\n");
+        fprintf(output, "%s\n",lista_palabras[i]);
         free(lista_palabras[i]);
     }
     fclose(archivo);
